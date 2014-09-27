1a8dc9e381d86c0f0d5beb6902837c96f0f5cf00
Frahaan Hussain <Sonar Systems>
2014-09-27 09:30:12 +0100

    Sequence

---
+++ src/app.js
@@ -0,0 +1,38 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        this.addChild(sprite, 0);
+
+        var sprite_action = cc.MoveBy.create(2.0, cc.p(50, 100));
+        var sprite_action2 = cc.FadeTo.create(2.0, 0);
+
+		var sequence_action = cc.Sequence.create(sprite_action, sprite_action2);
+
+        sprite.runAction(sequence_action);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
